1214e5932614a96ec0e05f0800a3f5913e8f34de
Andre-Souza539 <aljs.junca@gmail.com>
2026-08-04 08:22:49 -0300

    first commit

---
+++ src/main/java/br/dev/nerdlab/lunor/LunorApplication.java
@@ -0,0 +1,13 @@
+package br.dev.nerdlab.lunor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/nerdlab/lunor/LunorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.nerdlab.lunor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
